a57b1ec84fadb66bd09c127bbb63fb01d22049a9
Aleksit0 <aleksapython@gmail.com>
2021-01-29 08:22:36 +0100

    Working

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-function insert(num) {
-    document.form.textview.value += num;
-}
-function equal() {
-    document.form.textview.value = eval(document.form.textview.value);
-}
-function clean() {
-    document.form.textview.value = '';
-}
+var inputLabel = document.getElementById('inputLabel');
+     
+    function pushBtn(obj) {
+         
+        var pushed = obj.innerHTML;
+         
+        if (pushed == '=') {
+            // Calculate
+            inputLabel.innerHTML = eval(inputLabel.innerHTML);
+             
+        } else if (pushed == 'AC') {
+            // All Clear
+            inputLabel.innerHTML = '0';
+             
+        } else {
+            if (inputLabel.innerHTML == '0') {
+                inputLabel.innerHTML = pushed;
+                 
+            } else {
+                inputLabel.innerHTML += pushed;   
+            }
+        }
+    }
